16cd3869ea44023448329a51c4f435c71b319299
dunya <issatraore0112@gmail.com>
2025-12-23 20:51:07 +0000

    Ajout du package redis

--- music_store/settings.py
+++ music_store/settings.py
@@ -225,7 +225,7 @@ LOGGING = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = "django-db"
