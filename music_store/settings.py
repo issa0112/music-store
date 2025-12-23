24d84e2cde823db53956905bb86db7d7b88ec389
dunya <issatraore0112@gmail.com>
2025-12-23 15:34:59 +0000

    Ajout django-celery-results et django-celery-beat

--- music_store/settings.py
+++ music_store/settings.py
@@ -229,3 +229,6 @@ CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = "django-db"
+
+CELERY_CACHE_BACKEND = "django-cache" # Scheduler pour Celery Beat (stockage en base via django_celery_beat) 
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
