70446daf591aa9c35753d6b4133e5991b94f5c49
dunya <issatraore0112@gmail.com>
2025-12-23 23:45:15 +0000

    Add build script for Render

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Mettre à jour pip et outils de build
+pip install --upgrade pip setuptools wheel
+
 # Installer FFmpeg et autres dépendances système
 apt-get update && apt-get install -y ffmpeg
 
@@ -8,7 +11,9 @@ apt-get update && apt-get install -y ffmpeg
 pip install -r requirements.txt
 
 # Appliquer les migrations
-python manage.py migrate
+python manage.py migrate --noinput
+python manage.py migrate django_celery_results --noinput
+python manage.py migrate django_celery_beat --noinput
 
 # Collecter les fichiers statiques
 python manage.py collectstatic --noinput
