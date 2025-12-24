320a49c93cce3c1c15825a5d2e3a9a23105be563
dunya <issatraore0112@gmail.com>
2025-12-24 01:07:52 +0000

    Optimize Celery worker for low memory

--- music_store/celery.py
+++ music_store/celery.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "music_store.settings")
@@ -6,3 +7,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "music_store.settings")
 app = Celery("music_store")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+# 🔐 Configuration SSL pour Redis (rediss://)
+app.conf.broker_use_ssl = {
+    "ssl_cert_reqs": ssl.CERT_REQUIRED
+}
+
+app.conf.redis_backend_use_ssl = {
+    "ssl_cert_reqs": ssl.CERT_REQUIRED
+}
